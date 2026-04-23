3354778d52688a9adb64c8fdb50df244e403d84e
Arup Barik <arupbarik323@gmail.com>
2026-04-23 22:12:43 +0530

    just learn switch cases

--- FUNCTIONS_AND_RECURSION.C
+++ FUNCTIONS_AND_RECURSION.C
@@ -1,5 +1,6 @@
 #include <stdio.h>
     int main(){
         printf("lets start");
+        
         return 0;
     }
--- test.c
+++ test.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 int main(){
-    int a=2,b=3,c,d;
-    a++;
-    ++b;
-    printf("\n a=%d b=%d\n",a,b);
+    printf("lets start\n");
+    int a;
+    scanf("\n%d", &a);
+    switch(a){
+        case 23:
+        printf("the value is 23");
+        break;
+        case 45:
+        printf("the value is 45");          
+        break;
+        default:
+        printf("the value is neither 23 nor 45");
+    }
     return 0;
 }
 // dont execute this code, just check the syntax and logic.
