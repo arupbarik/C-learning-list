e12e232b550c75a1e03e1c8511c9e253217910ac
Arup Barik <arupbarik323@gmail.com>
2026-04-25 00:01:28 +0530

    factorial question

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
     printf("lets start\n");
+    // switch case
     int a;
     scanf("\n%d", &a);
     switch(a){
@@ -13,6 +14,21 @@ int main(){
         default:
         printf("the value is neither 23 nor 45");
     }
+    // N no. factorial
+    int n, i;
+    long long fac = 1;
+    printf("enter the value of n\n");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            fac = fac * i;
+        }
+        printf("Factorial of %d is %lld\n", n, fac);
+    }
+    
+
     return 0;
 }
-// dont execute this code, just check the syntax and logic.
