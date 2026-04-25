93a8b2d4ca47cc0f77b683dedc233f91797c9c80
Arup Barik <arupbarik323@gmail.com>
2026-04-25 21:21:12 +0530

    add the example of function and recursion

--- FUNCTIONS_AND_RECURSION.C
+++ FUNCTIONS_AND_RECURSION.C
@@ -1,7 +1,26 @@
 #include <stdio.h>
-
+int add(int a,int b);
+int factorial(int n);
     int main(){
-        printf("lets start");
-        
+        printf("lets start\n");
+    // test the functions
+     int result=add(10,20);
+     printf("%d\n",result);
+     // test the recursion
+     int n;
+     scanf("%d",&n);  
+     printf("%d", factorial(n)); 
         return 0;
     }
+int add(int a,int b){
+    return a+b;
+
+}
+int factorial(int n){
+    if (n==0){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+    }
+}
