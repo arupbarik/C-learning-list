4416de5e1d4e61a729a5a85f3b701c74d7f013ea
Arup Barik <arupbarik323@gmail.com>
2026-04-26 22:26:04 +0530

    ignore it

--- test.c
+++ test.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main(){
     printf("lets start\n");
-    int a;
-    scanf("\n%d", &a);
-    switch(a){
-        case 23:
-        printf("the value is 23");
-        break;
-        case 45:
-        printf("the value is 45");          
-        break;
-        default:
-        printf("the value is neither 23 nor 45");
-    }
+    // int a;
+    // scanf("\n%d", &a);
+    // switch(a){
+    //     case 23:
+    //     printf("the value is 23");
+    //     break;
+    //     case 45:
+    //     printf("the value is 45");          
+    //     break;
+    //     default:
+    //     printf("the value is neither 23 nor 45");
+    // }
+    
     return 0;
 }
 // dont execute this code, just check the syntax and logic.
