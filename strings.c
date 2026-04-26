c0ed20484fd9129ae6404b0343a662dcff83697a
Arup Barik <arupbarik323@gmail.com>
2026-04-26 22:23:16 +0530

    complete string but only string function left

--- strings.c
+++ strings.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
     printf("lets start\n");
+    //print string
+    char greeting[]="hello world";
+    printf("%s\n", greeting);
+
+    //input
+    char input[20];
+    fgets(input,sizeof(input),stdin);
+    printf("%s",input);
+
+    //2D array string
+    char fruits[3][20] = {"Apple", "Banana", "Cherry"};
+
+for (int i = 0; i < 3; i++)
+    printf("%s\n", fruits[i]);
+
+    // some string function
+    
+
+    return 0;
+    
 }
