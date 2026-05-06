237b4e4fd15dba9860c7ccc929b3636ff2d9ab1e
Arup Barik <arupbarik323@gmail.com>
2026-05-06 23:19:36 +0530

    started 2 no. ques

---
+++ lab2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    // 2.​
+    //      a.​ Write a program in C to calculate factorial value of a number.
+    //      b.​ Write a program in C to calculate XY without using pow( ) function.
+    //      c.​ Write a program in C to print all even and odd numbers between 1 and N.
+    //      d.​ Write a program in C to Print multiplication table (1–10) using nested loops
+    //      e.​ Write a program in C to calculate the number of digits present in a number.
+    //      f.​ Write a program in C to calculate the product of digits of a number.
+    //      g.​ Write a program in C to reverse a number.
+    //      h.​ Write a program in C to check whether a number is palindrome or not.
+    //      i.​ Write a program in C to determine whether a number is prime or not.
+    //      j.​ Write a program in C to Implement menu-driven calculator using infinite loop and
+    //      s     witch case
+    //      k.​ Write a C program to find HCF (GCD) of two numbers.
+    //      l.​ Write a C program to find LCM of two numbers.
+    //      m.​ Write a program to read N positive integer numbers; if any input is zero, program
+    //      should take the input again but if any input is negative loop body should be
+    //      terminated.
+    //      n.​ Write a program to Keep taking input and stop when sum exceeds 100.
+    //      o.​ Write a program in C to Find digital root of a number.The digital root of a number is
+    //      the single-digit value obtained by repeatedly adding the digits of the number until
+    //      only one digit remains. Example :Number = 456
+    //      i.​ 4 + 5 + 6 = 15
+    //      ii.​ 1 + 5 = 6
+    //      iii.​ Digital root = 6
+    //      p.​ Write a program in C to Convert decimal to binary using loop.
+
+    //q a.
+    // int fact,m=1,a=1;
+    // printf("Enter the value\n");
+    // scanf("%d",&fact);
+    // for (int i = 0; i < fact; i++)
+    // {
+    //     a=a*m;
+    //     m++;
+    // }
+    // printf("the factorial is %d\n",a);
+
+    //q b
+    // int x,y,l=1;
+    // printf("Enter two numbers:\n");
+    // scanf("%d %d", &x,&y);
+    // for (int i1 = 0; i1 < y; i1++)
+    // {
+    //     l=l*x;
+    // }
+    // printf("here is the result:%d\n",l);
+
+    //q c
+    // int num;
+    // printf("enter the range from 1 to N:\n");
+    // scanf("%d",&num);
+    // if (num>0)
+    // {
+    //     for (int i2 = 1; i2 <= num; i2++)
+    // {
+    //     if (i2%2==0)
+    //     {
+    //         printf("even:%d\n",i2);
+    //     }
+    //     else
+    //     {
+    //         printf("odd:%d\n",i2);
+    //     }
+    // }
+    // }
+    // else
+    // {
+    //     printf("invalid input\n");
+    // }
+    
+    //q d
+    // int mul,v;
+    // printf("desire number table:\n");
+    // scanf("%d",&mul);
+    // for (int i3 = 1; i3 <= 10; i3++)
+    // {
+    //     v=mul*i3;
+    //     printf("%d * %d = %d\n",mul,i3,v);
+    // }
+
+    //q e
+
+    
+    
+
+}
