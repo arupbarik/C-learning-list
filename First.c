d67e44a8e70dc487b77cdaf4b7f50f570ee7d8f5
Arup Barik <arupbarik323@gmail.com>
2026-05-09 01:09:00 +0530

    huhaha

--- First.c
+++ First.c
@@ -25,10 +25,39 @@ int main()
      }*/
     //%d is called format specifier
     //%d is for int,%f is for float,%c is for char
-    int i=10;
-    int j=i;
-    printf("the value i is %d and the value of j is %d",i,j);
+    int q=10;
+    int g=20;
+    printf("the value f is %d and the value of g is %d\n",q,g);
     
+    // S=1+(1+2)+(1+2+3)+(1+2+3+4)+.......+(1+2+3+4+.....+n)
+    
+    //my code
+    int n1,p=0,f=0,s=1;
+    printf("Enter term:\n");
+    scanf("%d",&n1);
+    for (int i = 0; i < n1; i++)
+    {
+        f=f+p+s;
+        p=p+s;
+        s++;
+    }
+    printf("%d\n",f);
+
+    //teachers code
+    int n,sum,s1;
+    printf("Enter term:\n");
+    scanf("%d",&n);
+    sum=0;
+    for (int i1 = 1; i1 <= n; i1++)
+    {
+        s1=0;
+        for (int j = 1; j <= i1; j++)
+        {
+            s1=s1+j;
+        }
+        sum=sum+s1;
+    }
+    printf("%d\n",sum);
 
     return 0;
 }
