95f7d428dc3dd608986c7151560bd42cc3cf9656
Arup Barik <arupbarik323@gmail.com>
2026-05-17 17:04:39 +0530

    all c tpics cover now just practice remaining

--- structure.c
+++ structure.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct student{
+    char name[50];
+    int age;
+    int class;
+};
 int main(){
     printf("lets start\n");
-    int n;
-    scanf("%d",&n);
-    //
+    struct student s1;
+    strcpy(s1.name,"Arup");
+    s1.age=19;
+    s1.class=1;
+    printf("%s\t",s1.name);
+    printf("%d\t",s1.age);
+    printf("%d\n",s1.class);
 }
