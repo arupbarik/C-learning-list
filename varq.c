134dd6df7a1290e88cf957702b872998c02eaa43
Arup Barik <arupbarik323@gmail.com>
2026-05-19 00:35:31 +0530

    sTART

---
+++ varq.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    //q1 Declare an int, float, and char. Print all three with correct format specifiers.
+    int a=10;
+    float b=58;
+    char c='a';
+    printf("%d\n",a);
+    printf("%.2f\n",b);
+    printf("%c\n",c);
+}
