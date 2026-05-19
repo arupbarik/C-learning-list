8073af819f05a154112a3b318d904af419bde2f6
Arup Barik <arupbarik323@gmail.com>
2026-05-20 01:07:05 +0530

    conditiona 1st ques done

---
+++ conditionalq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    //q1 Read a number and print whether it is positive, negative, or zero.
+    int a;
+    printf("enter the number\n");
+    scanf("%d",&a);
+    if (a==0)
+    {
+        printf("The number is zero\n");
+    }
+    else if (a>0)
+    {
+        printf("The number is positive\n");
+    }
+    else
+    {
+        printf("The number is negative\n");
+    }
+
+    //q2 Read two numbers and print the larger one.
+    
+}
--- sp.c
+++ sp.c
@@ -1,10 +1,12 @@
-// #include <stdio.h>
-// int main() {
-//         int i,j,m=5;
-//         for(i=1;i<=5;i++){
-//             for(j=1;j<=5;j++){
-//                 printf("%d\t",m);
-//             }
-//             printf("\n");
-//         }
-//     }
+#include <stdio.h>
+int main() {
+    int b,c;
+    printf("enter the numbers\n");
+    scanf("%d %d",&b,&c);
+    if (c>b)
+    {
+        /* code */
+    }
+    
+        
+    }
