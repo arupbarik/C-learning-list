453dab2dea71159832307151bb4a29292337836c
Arup Barik <arupbarik323@gmail.com>
2026-05-28 13:54:55 +0530

    permutation question

--- sp.c
+++ sp.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+
+int p(int n, int r) {
+    if (r == 0)
+        return 1;
+    else
+        return n * p(n - 1, r - 1);
+}
+
 int main() {
-    
-        
-    }
+    int a, b;
+    printf("Enter n then r: ");
+    scanf("%d %d", &a, &b);
+    printf("P(%d, %d) = %d\n", a, b, p(a, b));
+    return 0;
+}
